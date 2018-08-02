12565f9cb34f49ac91e629abe34b7eca07a29e8e
sthobis <s.thobi.sinaga@gmail.com>
2018-08-03 00:57:27 +0700

    feat: add welcome screen

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,36 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import ChatRoom from "./ChatRoom";
+import WelcomeScreen from "./WelcomeScreen";
 
 class App extends Component {
   state = {
     user: {
-      name: new Date().getTime(),
-      color: "#bada55"
-    }
+      name: localStorage.getItem("name") || `AnakBawang${Math.ceil(Math.random() * 100)}`,
+      color: localStorage.getItem("color") ||  "#fc5c65"
+    },
+    ready: false
   }
 
+  setUser = user => {
+    user && localStorage.setItem("name", user.name)
+    user && localStorage.setItem("color", user.color)
+    this.setState({ user })
+  }
+
+  setReadyStatus = ready => this.setState({ ready })
+
   render() {
-    const { user } = this.state
+    const { user, ready } = this.state
     return (
-      <ChatRoom user={user} />
+      <Fragment>
+        {
+          ready ? (
+            <ChatRoom user={user} setReadyStatus={this.setReadyStatus} />
+          ) : (
+            <WelcomeScreen user={user} setUser={this.setUser} setReadyStatus={this.setReadyStatus} />
+          )
+        }
+      </Fragment>
     );
   }
 }
--- src/components/WelcomeScreen.js
+++ src/components/WelcomeScreen.js
@@ -1,5 +1,61 @@
-import React from "react";
+import React, { Component } from "react";
 
-const WelcomeScreen = () => (<div></div>)
+// https://flatuicolors.com/palette/de
+const colorPalette = [
+  "#fc5c65","#fd9644","#fed330","#26de81","#2bcbba","#eb3b5a","#fa8231","#f7b731","#20bf6b","#0fb9b1","#45aaf2","#4b7bec","#a55eea","#d1d8e0","#778ca3","#2d98da","#3867d6","#8854d0","#a5b1c2","#4b6584"
+]
+
+class WelcomeScreen extends Component {
+
+  setUserProps = (prop, value) => {
+    const { user, setUser } = this.props
+    setUser({
+      ...user,
+      [prop]: value
+    })
+  }
+
+  render() {
+    const { user, setReadyStatus } = this.props
+    return (
+      <div className="container" style={{ backgroundColor: user.color }}>
+        <form
+          className="user-form"
+          onSubmit={e => setReadyStatus(true) }
+        >
+          <div className="field">
+            <input
+              autoFocus
+              type="text"
+              id="username"
+              name="username"
+              value={user.name}
+              onChange={e => this.setUserProps("name", e.target.value)}
+              placeholder="e.g. Anak Bawang"
+              maxLength={30}
+            />
+          </div>
+          <div className="field">
+            <label>Color</label>
+            <div className="color-palette">
+              {
+                colorPalette.map(color => (
+                  <span
+                    key={color}
+                    style={{ background: color }}
+                    onClick={e => this.setUserProps("color", color)}
+                  />
+                ))
+              }
+            </div>
+          </div>
+          <div className="field">
+            <button></button>
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
 
 export default WelcomeScreen
